7be8ae4b943535f62809ceb81893c3a6fb9008f8
aq020 <247498847@qq.com>
2021-08-16 01:20:37 +0800

    pt-vue1

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <pt/>
+  </div>
+</template>
+
+<script>
+import pt from './components/pt'
+
+export default {
+  name: 'App',
+  components: {
+    pt
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
